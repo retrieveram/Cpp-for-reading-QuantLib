8fed02ba8d26ea6b6bbe48f027de1f87a716bd22
retrieveram <151920356+retrieveram@users.noreply.github.com>
2025-09-23 20:10:50 +0900

    Add files via upload

---
+++ p069/p069.cpp
@@ -0,0 +1,13 @@
+﻿// p069.cpp
+#include <iostream>
+using namespace std;
+void main()
+{
+	int * xx, * yy;
+	xx = new int    ;       yy = new int;	
+	cout << "*xx は "; 	    cin >> *xx  ;
+	cout << "*yy = "; 	    cin >> *yy  ;
+	cout << "*xx + *yy = "  
+					<< *xx + *yy << endl;
+	delete xx; 	            delete yy   ; 
+}
