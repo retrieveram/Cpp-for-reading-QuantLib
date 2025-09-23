f7614c4e22f8db05ba348dfae8fd3f1d48be93dc
retrieveram <151920356+retrieveram@users.noreply.github.com>
2025-09-24 07:59:55 +0900

    Add files via upload

---
+++ 0312-p055-dPtr/ptrSG.cpp
@@ -0,0 +1,20 @@
+﻿// ptrSG.cpp  (pointer Single)
+#include <iostream>
+#include <string>
+using namespace std;
+
+class clsA {
+     string* ptr;
+   public:
+     clsA(string* xx) : ptr(xx) {}    
+     void showA() {cout << " *ptr: " << *ptr << endl;}
+};
+
+void main() {
+    string  a="AA";   string* pa = &a;
+    clsA cc(pa);      
+    cc.showA();
+
+    string  b="BB";   pa = &b; 
+    cc.showA();  
+}
