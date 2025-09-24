60c4f05c8bef045f9f7d9f40186fd1baaaa5fa32
retrieveram <151920356+retrieveram@users.noreply.github.com>
2025-09-24 20:26:06 +0900

    Add files via upload

--- 0401-normalPTR/nPTR.cpp
+++ 0401-normalPTR/nPTR.cpp
@@ -7,10 +7,10 @@ using namespace std;
 class clsA {
      string dataA;
    public:
-     clsA()         {cout          << "ﾃﾞﾌｫﾙﾄｺﾝｽﾄﾗｸﾀ生成:";}
+     clsA()         {cout          << "ﾃﾞﾌｫﾙﾄｺﾝｽﾄﾗｸﾀで'生成':";}
      clsA(string xx) 
-         : dataA(xx){cout << dataA << "でclsA生成:"      ;}
-     ~clsA()        {cout          << "clsA破棄"  << endl;}
+         : dataA(xx){cout << dataA << "で'clsA生成':"      ;}
+     ~clsA()        {cout          << "'clsA破棄'"  << endl;}
 
      void setA(string yy) {dataA = yy;}     
      void showA() {cout << " dataA: " << dataA << endl;}
@@ -18,22 +18,22 @@ class clsA {
 
 int main() {
      // デフォルトコンストラクタの例
-   { cout << "1. "                ; clsA obj0; 
+   { cout << "1. obj0を"          ; clsA obj0   ; 
      cout << " obj0の"            ; obj0.showA();
      cout << "2. ﾌﾞﾛｯｸ終了でobj0の";
    }
      // newでclsAのポインタptr0を作成
-     cout << "3. newと引数"       ; clsA * ptr0 = new clsA("AA"); 
+     cout << "3. ptr0をnewと引数" ; clsA * ptr0 = new clsA("AA"); 
      cout << " ptr0の"            ; ptr0->showA();
 
      // ptr1作成と ptr1 = ptr0 の動作
-   { clsA* ptr1 = new clsA()      ; ptr1 = ptr0     ;
-                                    ptr1->setA("BB");
-     cout << "4. ptr0は修正され"   ; ptr0->showA()   ;
+   { cout << "4. ptr1を"; clsA* ptr1 = new clsA(); ptr1 = ptr0     ;
+     cout << " ptr1=ptr0とする"  <<endl          ;  ptr1->setA("BB"); 
+     cout << "5. ptr1->..によりptr0は修正され"    ; ptr0->showA()    ;
    }
-     cout << "5. ﾌﾞﾛｯｸ終了後も上書き前のptr1の"
+     cout << "6. ﾌﾞﾛｯｸ終了後も上書き前のptr1の"
           << "ｵﾌﾞｼﾞｪｸﾄは破棄されない。ﾒﾓﾘﾘｰｸ発生。"     << endl; 
     // 終了直前
-    cout << "6. delete ptr0により" ; delete ptr0;
-    cout << "7. delete ptr0後で"   ; ptr0->showA();
+    cout << "7. delete ptr0により" ; delete ptr0;
+    cout << "8. delete ptr0後で"   ; ptr0->showA();
 }
---
+++ 0402-shrPTR-mk/sPTR.cpp
@@ -0,0 +1,40 @@
+﻿// sPTR.cpp
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class clsA {
+     string dataA;
+   public:
+     clsA()         {cout          << "ﾃﾞﾌｫﾙﾄｺﾝｽﾄﾗｸﾀで'生成':";}
+     clsA(string xx) 
+         : dataA(xx){cout << dataA << "で'clsA生成':"      ;}
+     ~clsA()        {cout          << "'clsA破棄'"  << endl;}
+
+     void setA(string yy) {dataA = yy;}     
+     void showA() {cout << " dataA: " << dataA << endl;}
+};
+
+int main() {
+     // newで作成したポインタをshared_ptrのコンストラクタに渡す
+     cout << "3つのｵﾌﾞｼﾞｪｸﾄの生成とｶｳﾝﾀｰ数#"   << endl;
+     shared_ptr<clsA> ptr0(  new clsA(    )  ); 
+     cout << " #"  << ptr0.use_count(); ptr0->showA(); 
+     shared_ptr<clsA> ptr1(  new clsA("AA")  );
+     cout << " #"  << ptr1.use_count(); ptr1->showA(); 
+     auto ptr2 = make_shared<clsA>   ("BB");
+     cout << " #"  << ptr2.use_count(); ptr2->showA();      
+     
+     // ptr1=ptr2の動作
+     cout << endl << "1. ptr1=ptr2でptr1の";
+     ptr1 = ptr2; 
+     cout << "2. ptr2のｶｳﾝﾀｰ #" << ptr2.use_count() << endl;
+     cout << "3. ptr1の"; ptr1->showA();
+     // ptr0=ptr2の動作
+     cout << "4. ptr0=ptr2でptr0の";     
+     ptr0 = ptr2;
+     cout << "5. ptr2のｶｳﾝﾀｰ #" << ptr2.use_count() << endl;
+     cout << "6. ptr0の"; ptr0->showA(); 
+     cout << "7. mainを終わるのでptr2の";     
+}
